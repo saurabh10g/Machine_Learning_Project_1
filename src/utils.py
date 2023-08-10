67b4966207488999f7831ce5831b315d1c1dc4bb
Saurabh Gadhave <gadhavesaurabh10@gmail.com>
2023-08-11 00:08:38 +0530

    pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,9 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
-def save_obj(file_path,obj):
+def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
@@ -16,13 +17,18 @@ def save_obj(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report ={}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[lsit(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=cv,n_jobs=n_jobs,verbose=verbose,refit=refit)
+            gs.fit(x_train,y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
             y_train_pred = model.predict(x_train)
@@ -37,3 +43,11 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
     
     except Exception as e:
         raise(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException
